0f2a180978fee2ac476c91548da2fe1b7d3eae44
az-ibr <ibrahhim.az@gmail.com>
2021-11-05 02:07:58 +0300

    change files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+    <header className='header'>
+        <img src ='https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg'>
+        </img>
+    </header>
+    <nav className='nav'>
+      <div>
+        <a>Profile</a>
+      </div>
+      <div>
+        <a>Messages</a>
+      </div>
+      <div>
+        <a>News</a>
+      </div>
+      <div>
+        <a>Music</a>
+      </div>
+      <div>
+        <a>Settings</a>
+      </div>
+    </nav>
+    <div className='content'>
+      <div>
+      <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
     </div>
-  );
-}
+    <div>
+      ava+description
+    </div>
+    <div>
+      My Posts
+    </div>
+    <div>
+      New Posts
+    </div>
+    <div>
+      Post1
+    </div>
+    <div>
+      Post2
+    </div>
+    </div>
+    </div>
 
+  )
+  }
+
+  
 export default App;
+
+
