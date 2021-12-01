826d474e66ae726a73ef917f2c37e698ae78d06a
az-ibr <ibrahhim.az@gmail.com>
2021-12-01 19:59:05 +0300

    add new file Header.module.css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,27 +1,21 @@
 import React from "react";
-const Profile =()=> {
-    return (
-        <div className='content'>
-      <div>
-      <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
-    </div>
-    <div>
-      ava+description
-    </div>
-    <div>
-      My Posts
-    </div>
-    <div>
-      New Posts
-    </div>
-    <div>
-      Post1
-    </div>
-    <div>
-      Post2
-    </div>
-    </div>
+import s from "./Profile.module.css";
 
-    )
-}
+// `${s.item} ${s.activ}`
+const Profile = () => {
+  return (
+    <div className={s.content}>
+      <div>
+        <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
+      </div>
+      <div>ava+description</div>
+      <div>My Posts</div>
+      <div>New Posts</div>
+      <div className="posts">
+        <div className={`${s.item} ${s.activ}`}>Post1</div>
+        <div className={s.item}>Post2</div>
+      </div>
+    </div>
+  );
+};
 export default Profile;
