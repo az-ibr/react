b51353745f4adc00e5080685ec0da830beae100a
Az I <85960036+az-ibr@users.noreply.github.com>
2022-03-14 01:01:50 +0300

    Add files via upload

---
+++ calc/app1.js
@@ -0,0 +1,44 @@
+// var ButtonPlus = document.getElementById ('ButtonPlus'); 
+// var ButtonMinus = document.getElementById ('ButtonMinus');
+// var ButtonMultiply = document.getElementById ('ButtonMultiply');
+// var ButtonDevide = document.getElementById ('ButtonDevide'); 
+
+var operationButtons = document.getElementsByClassName('operation-button'); 
+
+// var operationButtons = [ButtonPlus, ButtonMinus, ButtonMultiply, ButtonDevide];
+
+var input1 = document.getElementById ('number1');
+var input2 = document.getElementById ('number2'); 
+ 
+
+function makeOperation(operationCode) {
+   var number1 = Number(input1.value);
+   var number2 = Number(input2.value);
+   
+   if (operationCode==='+') {
+      var result = number1+number2;
+   } else if (operationCode === '-') {
+      var result = number1-number2;
+   } else if (operationCode=== '*') {
+      var result = number1*number2;
+   } else if (operationCode === '/') {
+      var result = number1/number2; 
+   } else {
+      window.alert('operation is unknown');
+   } 
+   window.alert(result);
+}
+ 
+
+
+function onOperationButtonClick(eventObject) {
+   var clickedElement = eventObject.currentTarget; 
+   var operation = clickedElement.innerHTML; 
+   makeOperation(operation); 
+}
+
+   for (var i=0; i<operationButtons.length; i++) {
+      var button = operationButtons[i]; 
+   button.addEventListener('click', onOperationButtonClick); 
+   }
+
